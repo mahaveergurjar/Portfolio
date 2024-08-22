1e8f622d7b637117a37b05c0ce0a59830677ae46
Mahaveer Gurjar <mahaveergurjar.work@gmail.com>
2024-08-22 22:07:27 +0530

    Update main.js

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -2,6 +2,7 @@ import Head from "next/head"
 import dynamic from "next/dynamic"
 import Navbar from "../navbar"
 import { Box, Container } from "@chakra-ui/react"
+import Footer from "../footer"
 import VoxelDogLoader from "../voxel-dog-loader"
 
 const LazyVoxelDog = dynamic(() => import("../voxel-dog"), {
@@ -14,12 +15,18 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Mahaveer's homepage" />
         <title>Mahaveer - Homepage</title>
       </Head>
+
       <Navbar path={router.asPath} />
+
       <Container maxW="container.md" pt={14}>
         <LazyVoxelDog />
+
         {children}
+
+        <Footer />
       </Container>
     </Box>
   )
