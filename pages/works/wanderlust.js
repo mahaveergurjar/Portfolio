264bf5e0b782f325e6fe5e79660096e73047d9bc
Mahaveer Gurjar <mahaveergurjar.work@gmail.com>
2024-08-23 00:02:58 +0530

    Update wanderlust.js

--- pages/works/wanderlust.js
+++ pages/works/wanderlust.js
@@ -1,52 +1,128 @@
+import { useState } from "react"
 import {
   Container,
   Badge,
   Link,
   List,
   ListItem,
-  AspectRatio,
+  Image,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  Box,
 } from "@chakra-ui/react"
 import { ExternalLinkIcon } from "@chakra-ui/icons"
 import { Title, WorkImage, Meta } from "../../components/work"
 import P from "../../components/paragraph"
 import Layout from "../../components/layouts/article"
 
-const Work = () => (
-  <Layout title="Wanderlust">
-    <Container>
-      <Title>
-        Wanderlust <Badge>2023-</Badge>
-      </Title>
-      <P>
-        Wandurlust Github Repository:WanderLust Developed a versile Website For
-        listing of villa for tourists Backend using Node Js, Express and mongodb
-        Frontend using EJS, CSS , HTML & bootstrap Also developed the website
-        error handling in frontend and backend also using middlewares
-      </P>
-      <List ml={4} my={4}>
-        <ListItem>
-          <Meta>Website</Meta>
-          <Link href="https://wanderlust-bxzn.onrender.com/listings">
-            https://wanderlust-bxzn.onrender.com/listings{" "}
-            <ExternalLinkIcon mx="2px" />
-          </Link>
-        </ListItem>
-        <ListItem>
-          <Meta>Platform</Meta>
-          <span>Windows/macOS/Linux/iOS/Android</span>
-        </ListItem>
-        <ListItem>
-          <Meta>Stack</Meta>
-          <span>NodeJS, EJS, bootstrap css, Express, Mongodb</span>
-        </ListItem>
-      </List>
+const Work = () => {
+  const [selectedImage, setSelectedImage] = useState(null)
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
-      <WorkImage src="/images/works/wanderlust1.png" alt="wnaderlust" />
-      <WorkImage src="/images/works/wanderlust2.png" alt="wanderlust" />
-      <WorkImage src="/images/works/wanderlust3.png" alt="wanderlust" />
-    </Container>
-  </Layout>
-)
+  const handleImageClick = src => {
+    setSelectedImage(src)
+    onOpen()
+  }
+
+  return (
+    <Layout title="Wanderlust">
+      <Container>
+        <Title>
+          Wanderlust <Badge>2024-</Badge>
+        </Title>
+        <P>
+          Wanderlust Github Repository: WanderLust Developed a versatile Website
+          For listing of villas for tourists. Backend using Node.js, Express,
+          and MongoDB, Frontend using EJS, CSS, HTML & Bootstrap. Also developed
+          the website with error handling in frontend and backend using
+          middlewares.
+        </P>
+        <List ml={4} my={4}>
+          <ListItem>
+            <Meta>Website</Meta>
+            <Link href="https://wanderlust-bxzn.onrender.com/listings">
+              https://wanderlust-bxzn.onrender.com/listings{" "}
+              <ExternalLinkIcon mx="2px" />
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Meta>Platform</Meta>
+            <span>Windows/macOS/Linux/iOS/Android</span>
+          </ListItem>
+          <ListItem>
+            <Meta>Stack</Meta>
+            <span>NodeJS, EJS, Bootstrap CSS, Express, MongoDB</span>
+          </ListItem>
+        </List>
+
+        <Box mb={4}>
+          <Image
+            src="/images/works/wanderlust1.png"
+            alt="wanderlust"
+            cursor="pointer"
+            onClick={() => handleImageClick("/images/works/wanderlust1.png")}
+            width="600px" // Adjust the width as needed
+            height="auto" // Adjust height to maintain aspect ratio
+            objectFit="cover"
+            borderRadius="md"
+          />
+        </Box>
+        <Box mb={4}>
+          <Image
+            src="/images/works/wanderlust2.png"
+            alt="wanderlust"
+            cursor="pointer"
+            onClick={() => handleImageClick("/images/works/wanderlust2.png")}
+            width="600px" // Adjust the width as needed
+            height="auto" // Adjust height to maintain aspect ratio
+            objectFit="cover"
+            borderRadius="md"
+          />
+        </Box>
+        <Box mb={4}>
+          <Image
+            src="/images/works/wanderlust3.png"
+            alt="wanderlust"
+            cursor="pointer"
+            onClick={() => handleImageClick("/images/works/wanderlust3.png")}
+            width="600px" // Adjust the width as needed
+            height="auto" // Adjust height to maintain aspect ratio
+            objectFit="cover"
+            borderRadius="md"
+          />
+        </Box>
+
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent maxW="70vw" maxH="100vh" overflow="hidden">
+            <ModalHeader>Image</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+            >
+              {selectedImage && (
+                <Image
+                  src={selectedImage}
+                  alt="Selected Image"
+                  maxW="100%"
+                  maxH="80vh"
+                  objectFit="contain"
+                  borderRadius="10"
+                />
+              )}
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+      </Container>
+    </Layout>
+  )
+}
 
 export default Work
-// export { getServerSideProps } from '../../components/chakra'
